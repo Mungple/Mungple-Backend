bbf9d2d42f02a7904cd527a10260bee8ef1a6418
홍성우 <sungwoo166@gmail.com>
2024-09-04 14:22:48 +0900

    :tada: :tada: :tada:

---
+++ src/main/java/com/e106/mungplace/MungplaceApplication.java
@@ -0,0 +1,14 @@
+package com.e106.mungplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MungplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MungplaceApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/e106/mungplace/MungplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e106.mungplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MungplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
