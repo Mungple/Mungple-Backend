9e05359460eea3b3e4c162ecbd242751441b6d82
SometimesThinks <ycw2859@gmail.com>
2024-09-12 01:48:02 +0900

    [#20] feat: 소켓 서버 커스텀 훅으로 변경

--- mungplace/src/services/socketService.ts
+++ mungplace/src/services/socketService.ts
@@ -1,51 +1,68 @@
+import {useState, useEffect} from 'react';
 import {Client} from '@stomp/stompjs';
 
-// Bear Token 가져오기
+const useWebSocket = () => {
+  const [clientSocket, setClientSocket] = useState<Client | null>(null);
 
-const socket = new Client({
-  brokerURL: 'ws://localhost:8080/ws',
-  connectHeaders: {
-    // Authorization: `Bearer ${token}`,
-  },
-  debug: str => {
-    console.log(str);
-  },
-  reconnectDelay: 5000,
-  // heartbeatIncoming: 4000,
-  // heartbeatOutgoing: 4000,
+  // Bear Token 가져오기
+  // const token = useSelector((state: RootState) => state.auth.token);
+  useEffect(() => {
+    const socket = new Client({
+      brokerURL: 'ws://localhost:8080/ws',
+      connectHeaders: {
+        // Authorization: `Bearer ${token}`,
+      },
+      debug: str => {
+        console.log(str);
+      },
+      reconnectDelay: 5000,
+      // heartbeatIncoming: 4000, // 서버로부터 메시지를 받는 주기
+      // heartbeatOutgoing: 4000, // 서버로 메시지를 보내는 주기
 
-  onConnect: () => {
-    console.log('소켓 연결 성공');
-    // 산책 기록 위치 수집
-    socket.subscribe('sub/explorations/{explorationId}', message => {
-      console.log('Received message from /topic/public:', message.body);
+      onConnect: () => {
+        console.log('소켓 연결 성공');
+        setClientSocket(socket);
+        // 산책 기록 위치 수집
+        socket.subscribe('sub/explorations/{explorationId}', message => {
+          console.log('산책 기록 위치 수집 에러 발생', message.body);
+        });
+        // 개인 블루존 조회
+        socket.subscribe('sub/users/{userId}/bluezone', message => {
+          console.log('개인 블루존 조회', message.body);
+        });
+        // 전체 블루존 조회
+        socket.subscribe('sub/bluezone', message => {
+          console.log('전체 블루존 조회', message.body);
+        });
+        // 전체 레드존 조회
+        socket.subscribe('sub/redzone', message => {
+          console.log('전체 레드존 조회', message.body);
+        });
+        // 마커 조회
+        socket.subscribe('sub/markers', message => {
+          console.log('마커 조회', message.body);
+        });
+      },
+
+      onStompError: frame => {
+        console.log('소켓 연결 에러 발생');
+        console.log('에러 메시지: ' + frame.headers['message']);
+        console.log('에러 상세 내용: ' + frame.body);
+      },
     });
-    // 개인 블루존 조회
-    socket.subscribe('sub/users/{userId}/bluezone', message => {
-      console.log('개인 블루존 조회', message.body);
-    });
-    // 전체 블루존 조회
-    socket.subscribe('sub/bluezone', message => {
-      console.log('Received message from /user/queue/errors:', message.body);
-    });
-    // 전체 레드존 조회
-    socket.subscribe('sub/redzone', message => {
-      console.log('Received message from /user/queue/errors:', message.body);
-    });
 
-    // 마커 조회
-    socket.subscribe('sub/markers', message => {
-      console.log('Received message from /user/queue/errors:', message.body);
+    socket.activate();
+    return () => {
+      socket.deactivate();
+    };
+  }, []);
+
+  const sendLocation = (explorationId: number, location: any) => {
+    clientSocket.publish({
+      destination: `pub/explorations/${explorationId}`,
+      body: JSON.stringify(location),
     });
-  },
+  };
+};
 
-  onStompError: frame => {
-    console.log('에러 메시지: ' + frame.headers['message']);
-    console.log('에러 상세 내용: ' + frame.body);
-  },
-});
-
-// 클라이언트 활성화
-socket.activate();
-
-export default socket;
+export default useWebSocket;
