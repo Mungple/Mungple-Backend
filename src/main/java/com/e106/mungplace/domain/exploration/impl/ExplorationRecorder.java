ba5480e0e0f50d0eaac0fba5d5ef385b083cbe70
이현수 <znight1020@naver.com>
2024-09-25 17:34:59 +0900

    [#44] feat: 9초동안 90m 이동 사용자 예외 처리

--- src/main/java/com/e106/mungplace/domain/exploration/impl/ExplorationRecorder.java
+++ src/main/java/com/e106/mungplace/domain/exploration/impl/ExplorationRecorder.java
@@ -2,44 +2,43 @@ package com.e106.mungplace.domain.exploration.impl;
 
 import com.e106.mungplace.common.map.dto.Point;
 import com.e106.mungplace.domain.user.impl.UserHelper;
+import com.e106.mungplace.domain.util.GeoUtils;
 import com.e106.mungplace.web.exception.ApplicationSocketException;
 import com.e106.mungplace.web.exception.dto.ApplicationSocketError;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-import java.util.Objects;
-import java.util.Set;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class ExplorationRecorder {
 
-    private String lastMungGeoHash;
-
     private final RedisTemplate<String, String> redisTemplate;
     private final UserHelper userHelper;
 
-    public void initRecord(String userId, String latitude, String longitude) {
-        String constantGeoHash = Point.toConstantGeoHash(latitude, longitude); // 일정 공간(30m x 19m) GeoHash
+    private String lastMungGeoHash;
 
-        redisTemplate.opsForSet().add("activeUsers", userId);
-        redisTemplate.opsForValue().set("users:" + userId + ":constant", constantGeoHash, 30, TimeUnit.MINUTES);
-    }
+    private final int MAX_DISTANCE_CAPACITY = 3;
+    private final int MAX_PRE_POINT_CAPACITY = 1;
 
-    public void endRecord(String userId) {
-        redisTemplate.opsForSet().remove("activeUsers", userId);
-        redisTemplate.opsForSet().remove("users:" + lastMungGeoHash, userId);
-        redisTemplate.expire("users:" + userId + ":geohash", 1, TimeUnit.MILLISECONDS);
-        redisTemplate.expire("users:" + userId + ":constant", 1, TimeUnit.MILLISECONDS);
-    }
+    private Deque<Double> distances = new ArrayDeque<>(MAX_DISTANCE_CAPACITY); // 3개 크기의 큐 (거리 저장)
+    private Deque<Point> prePoint = new ArrayDeque<>(MAX_PRE_POINT_CAPACITY); // 3개 크기의 큐 (이전 Point 저장)
 
     public void recordCurrentUserGeoHash(String userId, String latitude, String longitude) {
         String previousGeoHash = redisTemplate.opsForValue().get("users:" + userId + ":geohash");
 
-        if(previousGeoHash == null) throw new ApplicationSocketException(ApplicationSocketError.DONT_SEND_ANY_REQUEST);
+        if (previousGeoHash == null) {
+            throw new ApplicationSocketException(ApplicationSocketError.DONT_SEND_ANY_REQUEST);
+        }
 
         String userCurrentGeoHash = Point.toUserCurrentGeoHash(latitude, longitude); // 사용자 현재 위치 GeoHash
         String constantGeoHash = Point.toConstantGeoHash(latitude, longitude); // 일정 공간(30m x 19m) GeoHash
@@ -49,11 +48,55 @@ public class ExplorationRecorder {
         redisTemplate.opsForSet().add("users:" + mungGeoHash, userId);
 
         updateUserIdWhenGeoHashIsDifference(userId, previousGeoHash, userCurrentGeoHash, mungGeoHash); // 사용자의 현재 위치와, 위치 별 사용자 갱신
+        calculateAndRecordDistance(userId, latitude, longitude); // 사용자 이동 거리 계산
         verifyConstantPlace(userId, constantGeoHash); // 장시간 일정공간에 있는지 검증
     }
 
+    private void calculateAndRecordDistance(String userId, String latitude, String longitude) {
+        if (prePoint.isEmpty()) return;
+        Point currentPoint = new Point(Double.parseDouble(latitude), Double.parseDouble(longitude));
+        Point previousPoint = prePoint.poll();
+        double distance = GeoUtils.calculateDistance(previousPoint, currentPoint);
+
+        String amountDistanceString = redisTemplate.opsForValue().get("users:" + userId + ":distance");
+        double amountDistance = Double.parseDouble(amountDistanceString) + distance;
+        redisTemplate.opsForValue().set("users:" + userId + ":distance", String.valueOf(amountDistance), 60, TimeUnit.SECONDS);
+
+        if (distances.size() == MAX_DISTANCE_CAPACITY) distances.poll();
+        distances.add(distance);
+        prePoint.add(currentPoint);
+
+        log.info("총 이동거리 : {}", getTotalDistance());
+        if (getTotalDistance() >= 90.00) throw new ApplicationSocketException(ApplicationSocketError.TOO_FAST_EXPLORING);
+    }
+
+    public void initRecord(String userId, String latitude, String longitude) {
+        String userCurrentGeoHash = Point.toUserCurrentGeoHash(latitude, longitude);
+        String constantGeoHash = Point.toConstantGeoHash(latitude, longitude);
+
+        distances.clear();
+        prePoint.clear();
+        prePoint.add(new Point(Double.parseDouble(latitude), Double.parseDouble(longitude)));
+
+        redisTemplate.opsForSet().add("activeUsers", userId);
+        redisTemplate.opsForValue().set("users:" + userId + ":geohash", userCurrentGeoHash, 60, TimeUnit.SECONDS); // 사용자의 현재 GeoHash
+        redisTemplate.opsForValue().set("users:" + userId + ":distance", "0", 60, TimeUnit.SECONDS);
+        redisTemplate.opsForValue().set("users:" + userId + ":constant", constantGeoHash, 30, TimeUnit.MINUTES);
+    }
+
+    public void endRecord(String userId) {
+        redisTemplate.opsForSet().remove("activeUsers", userId);
+        redisTemplate.opsForSet().remove("users:" + lastMungGeoHash, userId);
+        redisTemplate.expire("users:" + userId + ":geohash", 1, TimeUnit.MILLISECONDS);
+        redisTemplate.expire("users:" + userId + ":distance", 1, TimeUnit.MILLISECONDS);
+        redisTemplate.expire("users:" + userId + ":constant", 1, TimeUnit.MILLISECONDS);
+
+    }
+
     private void updateUserIdWhenGeoHashIsDifference(String userId, String preGeoHash, String userCurrentGeoHash, String mungGeoHash) {
-        if (preGeoHash == null || Objects.equals(preGeoHash, userCurrentGeoHash)) return;
+        if (preGeoHash == null || Objects.equals(preGeoHash, userCurrentGeoHash)) {
+            return;
+        }
 
         String preMungGeoHash = preGeoHash.substring(0, 7);
         if (!Objects.equals(preMungGeoHash, mungGeoHash)) {
@@ -65,8 +108,9 @@ public class ExplorationRecorder {
     }
 
     private void removeEmptySet(String preMungGeoHash) {
-        if(redisTemplate.opsForSet().size("users:" + preMungGeoHash) == 0)
+        if (redisTemplate.opsForSet().size("users:" + preMungGeoHash) == 0) {
             redisTemplate.delete("users:" + preMungGeoHash);
+        }
     }
 
     private void verifyConstantPlace(String userId, String constantGeoHash) {
@@ -83,7 +127,10 @@ public class ExplorationRecorder {
 
     @Scheduled(fixedRate = 60000)
     private void verifyActiveUsers() {
-        if(userHelper.getAuthenticatedUser() == null) return;
+        if (SecurityContextHolder.getContext().getAuthentication() == null) {
+            return;
+        }
+
         Set<String> activeUsers = redisTemplate.opsForSet().members("activeUsers");
         String currentUserId = userHelper.getCurrentUserId().toString();
 
@@ -94,5 +141,11 @@ public class ExplorationRecorder {
                 throw new ApplicationSocketException(ApplicationSocketError.DONT_SEND_ANY_REQUEST);
             }
         });
+    }
+
+    private double getTotalDistance() {
+        double totalDistance = distances.stream().mapToDouble(Double::doubleValue).sum();
+        BigDecimal bd = BigDecimal.valueOf(totalDistance).setScale(2, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 }
--- src/main/java/com/e106/mungplace/domain/util/GeoUtils.java
+++ src/main/java/com/e106/mungplace/domain/util/GeoUtils.java
@@ -1,38 +1,48 @@
 package com.e106.mungplace.domain.util;
 
+import com.e106.mungplace.common.map.dto.Point;
 import org.gavaghan.geodesy.Ellipsoid;
 import org.gavaghan.geodesy.GeodeticCalculator;
+import org.gavaghan.geodesy.GeodeticCurve;
 import org.gavaghan.geodesy.GlobalCoordinates;
 import org.locationtech.spatial4j.context.SpatialContext;
 import org.locationtech.spatial4j.io.GeohashUtils;
 
-import com.e106.mungplace.common.map.dto.Point;
-
 public interface GeoUtils {
 
-	static Point calculateNorthWestPoint(Point point, int sideLength) {
-		GlobalCoordinates nw = calculateOffset(new GlobalCoordinates(point.lat(), point.lon()), -45,
-			sideLength / Math.sqrt(2));
+    static Point calculateNorthWestPoint(Point point, int sideLength) {
+        GlobalCoordinates nw = calculateOffset(new GlobalCoordinates(point.lat(), point.lon()), -45, sideLength / Math.sqrt(2));
 
-		return new Point(nw.getLatitude(), nw.getLongitude());
-	}
+        return new Point(nw.getLatitude(), nw.getLongitude());
+    }
 
-	static Point calculateSouthEastPoint(Point point, int sideLength) {
-		GlobalCoordinates nw = calculateOffset(new GlobalCoordinates(point.lat(), point.lon()), 135,
-			sideLength / Math.sqrt(2));
+    static Point calculateSouthEastPoint(Point point, int sideLength) {
+        GlobalCoordinates nw = calculateOffset(new GlobalCoordinates(point.lat(), point.lon()), 135, sideLength / Math.sqrt(2));
 
-		return new Point(nw.getLatitude(), nw.getLongitude());
-	}
+        return new Point(nw.getLatitude(), nw.getLongitude());
+    }
 
-	static Point calculateGeohashCenterPoint(String geoHashString) {
-		var center = GeohashUtils.decode(geoHashString, SpatialContext.GEO);
-		return new Point(center.getLat(), center.getLon());
-	}
+    static Point calculateGeohashCenterPoint(String geoHashString) {
+        var center = GeohashUtils.decode(geoHashString, SpatialContext.GEO);
+        return new Point(center.getLat(), center.getLon());
+    }
 
-	private static GlobalCoordinates calculateOffset(GlobalCoordinates center, double bearing, double distanceMeters) {
-		GeodeticCalculator geoCalc = new GeodeticCalculator();
-		Ellipsoid reference = Ellipsoid.WGS84;
+    static Double calculateDistance(Point previousPoint, Point currentPoint) {
+        GeodeticCalculator calculator = new GeodeticCalculator();
 
-		return geoCalc.calculateEndingGlobalCoordinates(reference, center, bearing, distanceMeters);
-	}
+        Ellipsoid reference = Ellipsoid.WGS84;
+        GlobalCoordinates start = new GlobalCoordinates(previousPoint.lat(), previousPoint.lon());
+        GlobalCoordinates end = new GlobalCoordinates(currentPoint.lat(), currentPoint.lon());
+
+        GeodeticCurve geoCurve = calculator.calculateGeodeticCurve(reference, start, end);
+
+        return geoCurve.getEllipsoidalDistance();
+    }
+
+    private static GlobalCoordinates calculateOffset(GlobalCoordinates center, double bearing, double distanceMeters) {
+        GeodeticCalculator geoCalc = new GeodeticCalculator();
+        Ellipsoid reference = Ellipsoid.WGS84;
+
+        return geoCalc.calculateEndingGlobalCoordinates(reference, center, bearing, distanceMeters);
+    }
 }
