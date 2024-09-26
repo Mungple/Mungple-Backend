8ccd52b27fe1b4f5637af7b4519d908cb5c75d01
홍성우 <sungwoo166@gmail.com>
2024-09-26 12:40:10 +0900

    [#49] feat: 유저 프로필 이미지 등록 기능 구현

--- src/main/java/com/e106/mungplace/web/user/service/UserService.java
+++ src/main/java/com/e106/mungplace/web/user/service/UserService.java
@@ -1,12 +1,18 @@
 package com.e106.mungplace.web.user.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.e106.mungplace.common.image.ImageRepository;
+import com.e106.mungplace.common.image.ImageStore;
+import com.e106.mungplace.common.transaction.GlobalTransactional;
+import com.e106.mungplace.domain.user.entity.User;
+import com.e106.mungplace.domain.user.impl.UserHelper;
+import com.e106.mungplace.domain.user.repository.UserRepository;
 import com.e106.mungplace.web.exception.ApplicationException;
 import com.e106.mungplace.web.exception.dto.ApplicationError;
+import com.e106.mungplace.web.user.dto.ImageNameResponse;
 import com.e106.mungplace.web.user.dto.UserInfoResponse;
-import com.e106.mungplace.domain.user.repository.UserRepository;
-import com.e106.mungplace.domain.user.impl.UserHelper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +24,8 @@ public class UserService {
 
 	private final UserRepository userRepository;
 	private final UserHelper userHelper;
+	private final ImageStore imageStore;
+	private final ImageRepository imageRepository;
 
 	public UserInfoResponse readUserInfo(Long userId) {
 		return userRepository.findById(userId)
@@ -27,5 +35,17 @@ public class UserService {
 				.imageName(targetUser.getImageName())
 				.build())
 			.orElseThrow(() -> new ApplicationException(ApplicationError.USER_NOT_FOUND));
+	}
+
+	@GlobalTransactional
+	public ImageNameResponse updateUserImage(MultipartFile imageFile) {
+		User user = userHelper.getCurrentUser();
+		String currentImageName = user.getImageName();
+		if(currentImageName != null) {
+			imageRepository.deleteImageById(currentImageName);
+		}
+		String userImage = imageStore.saveImage(imageFile);
+		user.updateImageName(userImage);
+		return new ImageNameResponse(userImage);
 	}
 }
