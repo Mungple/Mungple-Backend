2643805221f083c6cb2f3303f6fd6cfc211a196f
홍성우 <sungwoo166@gmail.com>
2024-09-26 12:40:52 +0900

    [#49] feat: 강아지 이미지 등록 기능 구현

--- src/main/java/com/e106/mungplace/web/dogs/controller/DogController.java
+++ src/main/java/com/e106/mungplace/web/dogs/controller/DogController.java
@@ -4,12 +4,23 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.e106.mungplace.web.dogs.dto.DogCreateRequest;
 import com.e106.mungplace.web.dogs.dto.DogResponse;
 import com.e106.mungplace.web.dogs.dto.DogUpdateRequest;
 import com.e106.mungplace.web.dogs.service.DogService;
+import com.e106.mungplace.web.user.dto.ImageNameResponse;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,30 +28,37 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/users")
 public class DogController {
 
 	private final DogService dogService;
 
 	@ResponseStatus(HttpStatus.CREATED)
-	@PostMapping("/users/dogs")
+	@PostMapping("/dogs")
 	public void findUserInfo(@RequestBody DogCreateRequest dogCreateRequest) {
 		dogService.createDogProcess(dogCreateRequest);
 	}
 
-	@GetMapping("/users/{userId}/dogs")
+	@GetMapping("/{userId}/dogs")
 	public ResponseEntity<List<DogResponse>> findDogsInfo(@PathVariable("userId") Long userId) {
 		return ResponseEntity.ok(dogService.findDogsProcess(userId));
 	}
 
-	@PutMapping("/users/dogs/{dogId}")
+	@PutMapping("/dogs/{dogId}")
 	public ResponseEntity<DogResponse> updateDogInfo(@PathVariable("dogId") Long dogId,
 		@RequestBody DogUpdateRequest dogUpdateRequest) {
 		return ResponseEntity.ok(dogService.updateDogProcess(dogId, dogUpdateRequest));
 	}
 
 	@ResponseStatus(HttpStatus.OK)
-	@DeleteMapping("/users/dogs/{dogId}")
+	@DeleteMapping("/dogs/{dogId}")
 	public void removeDog(@PathVariable("dogId") Long dogId) {
 		dogService.removeDogProcess(dogId);
+	}
+
+	@PostMapping(value = "/dogs/{dogId}/images", consumes = "multipart/form-data")
+	public ResponseEntity<ImageNameResponse> updateDogImage(@PathVariable("dogId") Long dogId,
+		@RequestPart(name = "image", required = true) MultipartFile imageFile) {
+		return ResponseEntity.ok(dogService.updateDogImageProcess(dogId, imageFile));
 	}
 }
