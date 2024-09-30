323603f7060d1ae6a7d9c865a4ff84d1b2482c9c
송재원 <fosong98@gmail.com>
2024-09-30 12:06:01 +0900

    [BE] fix#67: RequestMarkerType이 매핑되지 않는 문제 해결

--- src/main/java/com/e106/mungplace/web/marker/dto/MarkerSearchRequest.java
+++ src/main/java/com/e106/mungplace/web/marker/dto/MarkerSearchRequest.java
@@ -4,9 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 @Getter
+@Setter
 @Builder
 @RequiredArgsConstructor
 @ToString
