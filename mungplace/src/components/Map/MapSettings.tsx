147d6adb48b0ff726075c71bb3e111d1f24e234c
임경태 <limkt0925@gmail.com>
2024-10-01 16:15:20 +0900

    [#68] feat: setting 버튼 rngus

--- mungplace/src/components/Map/MapSettings.tsx
+++ mungplace/src/components/Map/MapSettings.tsx
@@ -1,3 +1,4 @@
+import {colors} from '@/constants'
 import React from 'react'
 import {FlatList, TouchableOpacity} from 'react-native'
 import Icon from 'react-native-vector-icons/Ionicons' // 아이콘 라이브러리
@@ -9,55 +10,100 @@ interface ButtonItem {
 }
 
 const buttonData: ButtonItem[] = [
-  {label: '블루존', onPress: () => console.log('블루존 버튼 클릭')},
-  {label: '레드존', onPress: () => console.log('레드존 버튼 클릭')},
-  {label: '멍플', onPress: () => console.log('멍플 버튼 클릭')},
-  {label: '편의정보', onPress: () => console.log('편의정보 버튼 클릭')},
-  {label: '내 블루존', onPress: () => console.log('내 블루존 버튼 클릭')},
-  {label: '레드마커', onPress: () => console.log('레드마커 버튼 클릭')},
-  {label: '공사장', onPress: () => console.log('공사장 버튼 클릭')},
-  {label: '블루 마커', onPress: () => console.log('블루 마커 버튼 클릭')},
+  {
+    label: '블루존',
+    onPress: () => {
+      console.log('블루존 버튼 클릭')
+    },
+  },
+  {
+    label: '레드존',
+    onPress: () => {
+      console.log('레드존 버튼 클릭')
+    },
+  },
+  {
+    label: '멍플',
+    onPress: () => {
+      console.log('멍플 버튼 클릭')
+    },
+  },
+  {
+    label: '편의정보',
+    onPress: () => {
+      console.log('편의정보 버튼 클릭')
+    },
+  },
+  {
+    label: '내 블루존',
+    onPress: () => {
+      console.log('내 블루존 버튼 클릭')
+    },
+  },
+  {
+    label: '레드마커',
+    onPress: () => {
+      console.log('레드마커 버튼 클릭')
+    },
+  },
+  {
+    label: '공사장',
+    onPress: () => {
+      console.log('공사장 버튼 클릭')
+    },
+  },
+  {
+    label: '블루 마커',
+    onPress: () => {
+      console.log('블루 마커 버튼 클릭')
+    },
+  },
 ]
 
 const CustomBottomSheetContent: React.FC = () => {
   const renderButtonItem = ({item}: {item: ButtonItem}) => (
-    <>
+    <ButtonContainer>
       <Button onPress={item.onPress}>
-        <Icon name="star-outline" size={24} color="black" />
+        <Icon name="star-outline" size={24} color="white" />
       </Button>
       <ButtonLabel>{item.label}</ButtonLabel>
-    </>
+    </ButtonContainer>
   )
 
   return (
-    <ButtonContainer>
-      <FlatList
-        data={buttonData}
-        renderItem={renderButtonItem}
-        keyExtractor={item => item.label}
-        numColumns={2} // 2개의 버튼을 한 행에 배치
-        contentContainerStyle={{paddingBottom: 10}}
-      />
-    </ButtonContainer>
+    <List
+      data={buttonData}
+      renderItem={renderButtonItem}
+      keyExtractor={item => item.label}
+      numColumns={2}
+      contentContainerStyle={{alignItems: 'center', paddingBottom: 40}}
+    />
   )
 }
 
-const ButtonContainer = styled.View`
-  padding: 10px;
-`
+const List = styled.FlatList`
+  width: 100%;
+` as unknown as typeof FlatList
 
 const Button = styled(TouchableOpacity)`
-  flex: 1;
-  background-color: #f0f0f0;
+  width: 70px;
+  height: 70px;
   margin: 10px;
-  padding: 20px;
-  border-radius: 10px;
+  padding: 10px;
+  border-radius: 35px;
   align-items: center;
   justify-content: center;
+  background-color: ${colors.GRAY_300};
+`
+
+const ButtonContainer = styled.View`
+  margin: 0 30px 30px;
+  align-items: center;
+  flex-direction: column;
 `
 
 const ButtonLabel = styled.Text`
-  margin-top: 10px;
+  margin-top: 5px;
   font-size: 16px;
   font-weight: bold;
   color: black;
