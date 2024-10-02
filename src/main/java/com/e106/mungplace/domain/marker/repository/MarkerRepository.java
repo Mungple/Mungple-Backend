1f5cfbf4570f72c50f4771269b939b682e35b0e6
홍성우 <sungwoo166@gmail.com>
2024-10-02 17:39:39 +0900

    [#76] feat: 마커 조회 구현

--- src/main/java/com/e106/mungplace/domain/marker/repository/MarkerRepository.java
+++ src/main/java/com/e106/mungplace/domain/marker/repository/MarkerRepository.java
@@ -1,10 +1,21 @@
 package com.e106.mungplace.domain.marker.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.e106.mungplace.domain.marker.entity.Marker;
 
-public interface MarkerRepository extends CrudRepository<Marker, UUID> {
+public interface MarkerRepository extends JpaRepository<Marker, UUID> {
+
+	@Query("SELECT m FROM Marker m WHERE m.user.userId = :userId ORDER BY m.createdDate DESC")
+	List<Marker> findFirstMarkersByUserId(@Param("userId") Long userId, Pageable pageable);
+
+	@Query("SELECT m FROM Marker m WHERE m.user.userId = :userId AND m.createdDate < :createdAt ORDER BY m.createdDate DESC")
+	List<Marker> findMarkersByUserIdAndCursor(@Param("userId") Long userId, @Param("createdAt") LocalDateTime createdAt, Pageable pageable);
 }
