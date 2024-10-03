9cfea0bc64c9b4d35ecf9c48dabcddd676a8d02e
jaedoo98 <fosong98@gmail.com>
2024-10-03 18:03:09 +0900

    [#79] feat: Loggable 애노테이션 생성

---
+++ src/main/java/com/e106/mungplace/common/log/Loggable.java
@@ -0,0 +1,19 @@
+package com.e106.mungplace.common.log;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.e106.mungplace.common.log.dto.LogAction;
+import com.e106.mungplace.common.log.dto.LogLevel;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Loggable {
+	LogLevel level();
+
+	LogAction action();
+
+	String message();
+}
