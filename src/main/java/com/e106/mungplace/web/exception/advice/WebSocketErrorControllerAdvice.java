db3771195b131b4497f22754a0d878f7b9540d48
송재원 <fosong98@gmail.com>
2024-10-06 22:46:38 +0900

    [#79] feat: Socket error 관련 로그 로직 작성

--- src/main/java/com/e106/mungplace/web/exception/advice/WebSocketErrorControllerAdvice.java
+++ src/main/java/com/e106/mungplace/web/exception/advice/WebSocketErrorControllerAdvice.java
@@ -13,6 +13,10 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
+import com.e106.mungplace.common.log.dto.LogAction;
+import com.e106.mungplace.common.log.dto.LogLevel;
+import com.e106.mungplace.common.log.dto.StompErrorLog;
+import com.e106.mungplace.common.log.impl.ApplicationLogger;
 import com.e106.mungplace.domain.exploration.impl.ExplorationRecorder;
 import com.e106.mungplace.web.exception.ApplicationSocketException;
 import com.e106.mungplace.web.exception.dto.ApplicationSocketError;
@@ -20,9 +24,7 @@ import com.e106.mungplace.web.exception.dto.ErrorResponse;
 import com.e106.mungplace.web.handler.interceptor.WebSocketSessionManager;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @ControllerAdvice
 @RequiredArgsConstructor
 public class WebSocketErrorControllerAdvice {
@@ -30,6 +32,7 @@ public class WebSocketErrorControllerAdvice {
 	private final WebSocketSessionManager sessionManager;
 	private final SimpMessagingTemplate messagingTemplate;
 	private final ExplorationRecorder explorationRecorder;
+	private final ApplicationLogger logger;
 
 	/* 지정되지 않은 소켓 예외인 경우(ES00) */
 	@MessageExceptionHandler(SocketException.class)
@@ -37,35 +40,41 @@ public class WebSocketErrorControllerAdvice {
 		sendErrorMessage(accessor, UN_RECOGNIZED_SOCKET_EXCEPTION);
 		postProcessIfFatalException(accessor);
 		sessionManager.closeSession(accessor.getSessionId());
+
+		errorLogging(accessor, UN_RECOGNIZED_SOCKET_EXCEPTION);
 	}
 
 	/* 요청 메시지 본문이 잘못된 경우(ES01) */
+
 	@MessageExceptionHandler({MethodArgumentNotValidException.class, IllegalArgumentException.class,
 		MessageConversionException.class})
 	public void handleInValidMessageBody(StompHeaderAccessor accessor) throws IOException {
 		sendErrorMessage(accessor, MESSAGE_BODY_NOT_VALID);
 		postProcessIfFatalException(accessor);
 		sessionManager.closeSession(accessor.getSessionId());
-	}
 
+		errorLogging(accessor, MESSAGE_BODY_NOT_VALID);
+	}
 	/* 일시적인 네트워크 오류 혹은 URL을 잘못 설정한 경우(ES02) */
+
 	@MessageExceptionHandler(MessageDeliveryException.class)
 	public void handleFailedMessageDelivery(StompHeaderAccessor accessor) throws IOException {
 		sendErrorMessage(accessor, MESSAGE_DELIVERY_FAILED);
 		postProcessIfFatalException(accessor);
 		sessionManager.closeSession(accessor.getSessionId());
-	}
 
+		errorLogging(accessor, MESSAGE_DELIVERY_FAILED);
+	}
 	/* 그 외 비즈니스 로직에서 지정할 수 있는 예외인 경우, ES 소켓 유지, FES 소켓 만료(현재 FES 는 산책밖에 없지만 추후 생긴다면 if 분리 ) */
+
 	@MessageExceptionHandler(ApplicationSocketException.class)
 	public void handleCustomException(ApplicationSocketException e, StompHeaderAccessor accessor) throws IOException {
 		sendErrorMessage(accessor, e.getError());
 		if (e.getError().getErrorCode().startsWith("FES")) {
 			postProcessIfFatalException(accessor);
 		}
-		log.error("예외 발생. [CODE] : {}, [MESSAGE] : {}", e.getError().getErrorCode(), e.getError().getMessage());
+		errorLogging(accessor, e.getError());
 	}
-
 	private void postProcessIfFatalException(StompHeaderAccessor accessor) throws IOException {
 		String userId = accessor.getUser().getName();
 		if(accessor.getSessionAttributes().containsKey("explorationId")) {
@@ -89,5 +98,10 @@ public class WebSocketErrorControllerAdvice {
 		} catch (InterruptedException e) {
 			throw new RuntimeException(e);
 		}
+	}
+
+	private void errorLogging(StompHeaderAccessor accessor, ApplicationSocketError error) {
+		StompErrorLog log = new StompErrorLog(accessor.getUser().getName(), error);
+		logger.log(LogLevel.ERROR, LogAction.SOCKET, log, getClass());
 	}
 }
