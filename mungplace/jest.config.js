e1ba8e9127cc316bc9421ad747132c1ca82a5d05
임경태 <limkt0925@gmail.com>
2024-10-06 22:57:10 +0900

    [#97] chore: install @testing-library/react-native

--- mungplace/jest.config.js
+++ mungplace/jest.config.js
@@ -3,9 +3,7 @@ module.exports = {
   transform: {
     '^.+\\.(js|ts|tsx)?$': 'babel-jest',
   },
-  transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation)/)',
-  ],
+  transformIgnorePatterns: ['node_modules/(?!(react-native|@react-native|@react-navigation)/)'],
   setupFilesAfterEnv: [],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
