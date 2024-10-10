b2a08ca0afee16c504debba2e310e6821f583e25
박지원 <tlsxp13@naver.com>
2024-10-10 15:39:16 +0900

    [#124] refactor : 자잘한 버그들

--- mungplace/src/screens/map/MarkerDetailScreen.tsx
+++ mungplace/src/screens/map/MarkerDetailScreen.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { View, Image, StyleSheet, ActivityIndicator, ScrollView } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { useUserStore } from '@/state/useUserStore'; // 유저 스토어
+import { colors } from '@/constants';
 import CustomText from '@/components/common/CustomText'; // 커스텀 텍스트
 import CustomButton from '@/components/common/CustomButton'; // 커스텀 버튼
 import { deleteMarker, getMarkerDetails } from '@/api';
@@ -42,7 +43,7 @@ const MarkerDetailScreen: React.FC = () => {
     }
   };
 
-  const handleDelete = (markerId : string ) => async () => {
+  const handleDelete =  async () => {
     const status = await deleteMarker(markerId);
 
     if (status === 202) {
@@ -143,16 +144,15 @@ const styles = StyleSheet.create({
     textAlign: 'right',
   },
   imageContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
+    alignItems: 'center',
     marginBottom: 20,
   },
   image: {
     width: '100%',
-    height: 200,
+    height: undefined,
+    aspectRatio: 1.5,
     borderRadius: 8,
-    marginBottom: 10,
     borderWidth: 1,
     borderColor: '#ecf0f1',
     resizeMode: 'cover',
