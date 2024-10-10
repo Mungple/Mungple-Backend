1c78b1e2e6274b19e033ab4fa2f17ea68f1c2786
송재원 <fosong98@gmail.com>
2024-10-10 17:59:03 +0900

    temp

--- scripts/heatmap-test.js
+++ scripts/heatmap-test.js
@@ -10,8 +10,8 @@ let receivedMessageCount = new Counter('received_message_count');
 let requestResponseDifferenceRate = new Rate('request_response_difference_rate');
 
 const startTestTime = new Date();
-const url = 'ws://host.docker.internal:8080/ws';
-const loginUrl = 'http://host.docker.internal:8080/manager/login';
+const url = 'ws://localhost:8080/ws';
+const loginUrl = 'http://localhost:8080/manager/login';
 
 // export const options = {
 //     stages: [
@@ -23,87 +23,28 @@ const loginUrl = 'http://host.docker.internal:8080/manager/login';
 // };
 
 export const options = {
-    vus: 100,
-    duration: '90s',
-};
-
-function getJwtToken(userId) {
-    const res = http.post(`${loginUrl}?username=manager${userId}`);
-    const token = res.json().accessToken;
-    check(res, {
-        'login successful': (r) => r.status === 200,
-        'token exists': (r) => token !== undefined,
-    });
-    return token;
-}
-
-function sendMessage(socket, userId) {
-    const currentTime = new Date();
-    const elapsedTime = (currentTime - startTestTime) / 1000;
-
-    if (elapsedTime > 30) return;
-
-    const requestId = Math.random().toString(36).substr(2, 9);
-    const startTime = new Date();
-
-    const message = JSON.stringify({
-        requestId: requestId,
-        side: 500,
-        point: {
-            lat: 35.09493885488935,
-            lon: 128.853454676335,
+    scenarios: {
+        contacts: {
+            executor: 'constant-vus',
+            vus: 10,
+            duration: '10s',
+            gracefulStop: '3s',
         },
-    });
-
-    const sendFrame = `SEND\ndestination:/pub/bluezone\ncontent-type:application/json\n\n${message}\0`;
-    socket.send(sendFrame);
-
-    requestTimestamps.set(requestId, startTime);
-
-    messageCounter.add(1);
-    requestResponseDifferenceRate.add(0);
-}
-
-function handleMessage(msg) {
-    if(msg.indexOf("cells") !== -1) return
-
-    const messageParts = msg.split('\n\n');
-    const requestId = messageParts[1].trim().slice(0, -1);
-
-    if (requestTimestamps.has(requestId)) {
-        const startTime = new Date(requestTimestamps.get(requestId));
-        const endTime = new Date();
-        const latency = (endTime - startTime);
-
-        receivedMessageCount.add(1);
-        messageLatency.add(latency);
-        requestResponseDifferenceRate.add(1);
-
-        requestTimestamps.delete(requestId);
     }
-}
+};
 
 export default function () {
     const userId = __VU;
     const token = getJwtToken(userId);
 
     const response = ws.connect(url + `?Authorization=${token}`, {}, function (socket) {
+
         socket.on('open', function () {
-            console.log(`User ${userId} connected to WebSocket server`);
+            handleConnect(socket, userId);
+        });
 
-            const connectFrame = 'CONNECT\naccept-version:1.1,1.0\nhost:localhost\n\n\0';
-            socket.send(connectFrame);
-
-            const subscribeFrame = `SUBSCRIBE\nid:sub-${userId}\ndestination:/user/sub/bluezone\n\n\0`;
-            socket.send(subscribeFrame);
-
-            socket.setInterval(function () {
-                sendMessage(socket, userId);
-            }, 10000);
-
-            socket.on('message', function (msg) {
-                handleMessage(msg);
-            });
+        socket.on('message', function (msg) {
+            handleMessage(msg);
         });
 
         socket.on('close', function () {
@@ -119,3 +60,72 @@ export default function () {
         'WebSocket connection successful': (r) => r && r.status === 101,
     });
 }
+
+
+function getSendFrame(message) {
+    return `SEND\ndestination:/pub/bluezone\ncontent-type:application/json\n\n${message}\0`;
+}
+
+function getConnectFrame() {
+    return 'CONNECT\naccept-version:1.1,1.0\nhost:localhost\n\n\0';
+}
+
+function getSubscribeFrame(userId) {
+    return `SUBSCRIBE\nid:sub-${userId}\ndestination:/user/sub/bluezone\n\n\0`;
+}
+
+
+function getJwtToken(userId) {
+    const res = http.post(`${loginUrl}?username=manager${userId}`);
+    const token = res.json().accessToken;
+    check(res, {
+        'login successful': (r) => r.status === 200,
+        'token exists': (r) => token !== undefined,
+    });
+    return token;
+}
+
+function sendMessage(socket) {
+    const currentTime = new Date();
+    const elapsedTime = (currentTime - startTestTime) / 1000;
+
+    if (elapsedTime > 30) return;
+
+    const requestId = Math.random().toString(36).substr(2, 9);
+    const startTime = new Date();
+
+    const message = JSON.stringify({
+        side: 500,
+        point: {
+            lat: 35.09493885488935,
+            lon: 128.853454676335,
+        },
+    });
+
+    const sendFrame = getSendFrame(message);
+    socket.send(sendFrame);
+
+    messageCounter.add(1);
+
+    requestTimestamps.set(requestId, startTime);
+    requestResponseDifferenceRate.add(false);
+}
+
+function handleConnect(socket, userId) {
+    console.log(`User ${userId} connected to WebSocket server`);
+
+    const connectFrame = getConnectFrame();
+    socket.send(connectFrame);
+
+    const subscribeFrame = getSubscribeFrame(userId);
+    socket.send(subscribeFrame);
+
+    sendMessage(socket, userId);
+}
+
+function handleMessage(msg) {
+    if(msg.startsWith("CONNECT")) return;
+
+    requestResponseDifferenceRate.add(true);
+}
+
--- scripts/test-k6.js
+++ scripts/test-k6.js
@@ -1,16 +1,35 @@
-import http from 'k6/http';
-import { check, sleep } from 'k6';
+import stomp from 'k6/x/stomp';
 
-export const options = {
-    stages: [
-        { duration: '30s', target: 20 },
-        { duration: '1m30s', target: 10 },
-        { duration: '20s', target: 0 },
-    ],
-};
+// connect to broker
+const client = stomp.connect({
+    addr: 'localhost:8080',
+    timeout: '2s',
+});
 
 export default function () {
-    const res = http.get('https://httpbin.test.k6.io/');
-    check(res, { 'status was 200': (r) => r.status == 200 });
-    sleep(1);
+    // send a message to '/my/destination' with text/plain as MIME content-type
+    client.send('my/destination', 'text/plain', 'Hello xk6-stomp!');
+
+    const subscribeOpts = {
+        ack: 'client' // client-individual or auto (default)
+    }
+    // subscribe to receive messages from 'my/destination' with the client ack mode
+    const subscription = client.subscribe('my/destination', subscribeOpts);
+
+    // read the message
+    const msg = subscription.read();
+
+    // show the message as a string
+    console.log('msg', msg.string());
+
+    // ack the message
+    client.ack(msg);
+
+    // unsubscribe from destination
+    subscription.unsubscribe();
+}
+
+export function teardown() {
+    // disconnect from broker
+    client.disconnect();
 }
